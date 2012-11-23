6016b7e9990a1aa313e68f5ec12a829f10a527e3
Max Hellqvist <max.hellqvist@elev.it-gymnasiet>
2012-11-23 10:24:40 +0100

    Change 1

--- game.rb
+++ game.rb
@@ -6,7 +6,19 @@ class Game < Chingu::Window
 		def initialize
 			super
 			self.input = {esc: :exit}
+			@player = Player.create
 		end
+end
+
+class Player < Chingu::GameObject
+
+	#meta-constructor
+	def setup
+		@x, @y = 350, 400
+		# @x = 350
+		# @y = 400
+		@image = Gosu::Image["squid.png"]
+	end
 end
 
 Game.new.show

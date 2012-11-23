60c23bade8d94b77973316757cf51e88c7d1fe49
Max Hellqvist <max.hellqvist@elev.it-gymnasiet>
2012-11-23 10:44:59 +0100

    Change 2

--- game.rb
+++ game.rb
@@ -18,6 +18,35 @@ class Player < Chingu::GameObject
 		# @x = 350
 		# @y = 400
 		@image = Gosu::Image["squid.png"]
+		self.input = {
+			holding_left: :left,
+			holding_right: :right,
+			holding_up: :up,
+			holding_down: :down
+		}
+	end
+	def left
+		unless @x - 28 <= 0
+		@x -= 3
+	end
+	end
+
+	def right
+		unless @x + 28 >= 800
+		@x += 3
+	end
+	end
+
+	def down
+		unless @y + 28 <= 0
+		@y += 3
+	end
+	end
+
+	def up
+		unless @y - 28 >= 600
+		@y -= 3
+	end
 	end
 end
 
